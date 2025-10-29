458e663aaba92fb144da3d0be0710199fd11f796
mitchell <mitchell>
2025-10-29 19:47:26 -0400

    bump xell offset count

--- libxenon/drivers/xb360/xb360.h
+++ libxenon/drivers/xb360/xb360.h
@@ -84,7 +84,7 @@ static const unsigned int vfusesOffsets[VFUSES_OFFSETS_COUNT] = { 0x95000,   // 
 #define XELL_FOOTER_LENGTH 16
 #define XELL_FOOTER "xxxxxxxxxxxxxxxx"
 
-#define XELL_OFFSET_COUNT         7
+#define XELL_OFFSET_COUNT         8
 static const unsigned int xelloffsets[XELL_OFFSET_COUNT] = {0x70000, // ggBoot main xell-gggggg
 															0x95060,   // FreeBOOT Single-NAND main xell-2f
 															0x100000,  // XeLL-Only Image (Main)
